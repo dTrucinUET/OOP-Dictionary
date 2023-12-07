0294a3177e9a1257473c517dc1c753318c472b08
VanHuyLuan <124419803+VanHuyLuan@users.noreply.github.com>
2023-12-07 12:22:55 +0700

    Delete src/main directory

--- src/main/java/com/example/dictionary/controller/FavoriteController.java
+++
@@ -1,8 +0,0 @@
-package com.example.dictionary.controller;
-
-/**
- * Hiển thị các từ yêu thih
- */
-
-public class FavoriteController extends MainController{
-}
--- src/main/java/com/example/dictionary/controller/TranslateController.java
+++
@@ -1,9 +0,0 @@
-package com.example.dictionary.controller;
-
-/**
- * Chỗ này t cần để dịch văn bản
- * Cần thêm cả cái button voice để đọc từ
- */
-
-public class TranslateController extends MainController{
-}
